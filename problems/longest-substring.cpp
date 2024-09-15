0bf97176a9307f4780db88e259b03014c34737c7
mwangimuringi <mwangimuringi10@gmail.com>
2024-09-15 17:09:49 +0300

    Corrected using unordered-map

--- problems/longest-substring.cpp
+++ problems/longest-substring.cpp
@@ -1,32 +1,27 @@
-/*
-Input: s = "eleetminicoworoep"
-Output: 13
-Explanation: The longest substring is "leetminicowor" which contains two each of the vowels: e, i and o and zero of the vowels: a and u.
-*/
-
-/* xor a^0 =a a^a=0*/
 #include <string>
 #include <iostream>
-#include <map>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
     int findTheLongestSubstring(string s) {
-        map<int, int>mp;
-        mp[0] = 1;
+        unordered_map<int, int> mp;
+        mp[0] = -1;
         int cnt = 0, ans = 0;
-        for (int i = 0; i < s.length(); i++)
-        {
-            if(s[i] == 'a' || s [i] == 'e' || s [i] == 'i' || s [i] == 'o' || s [i] == 'u')
-                cnt = cnt^(1<<((int)(s[i] - 'a')));
-            if(mp.count(cnt))
-            {
-                ans = max(ans, i - mp[cnt]);
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
+                cnt ^= (1 << (s[i] - 'a'));  // Toggle the bit corresponding to the vowel
             }
-            else
+
+            // Check if this bitmask has been seen before
+            if (mp.find(cnt) != mp.end()) {
+                ans = max(ans, i - mp[cnt]);
+            } else {
                 mp[cnt] = i;
+            }
         }
         return ans;
     }
