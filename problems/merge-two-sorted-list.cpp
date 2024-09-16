c8148c3426c00f9b436baedca6cec8046c77d97e
mwangimuringi <mwangimuringi10@gmail.com>
2024-09-16 14:21:17 +0300

    Merged both lists

--- problems/merge-two-sorted-list.cpp
+++ problems/merge-two-sorted-list.cpp
@@ -9,12 +9,37 @@
  *     T = O(N)            S = O(1)
  * };
  */
+class Solution
+{
 public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2)
     {
+        if( !list1 ) return list2;
+        if( !list2 ) return list1;
+
+        ListNode* head = list1;
+        if(list1->val > list2->val){
+            head = list2;
+            list2 = list2->next;
         }
         else
+            list1 = list1->next;
+        ListNode* cur = head;
+        while(list1 && list2)
+        {
+            if(list1->val < list2->val) {
+                cur->next = list1;
+                list1 = list1->next;
+            } else
             {
+                cur->next = list2;
+                list2 = list2->next;
             }
+            cur = cur->next;
+        };
+        if(!list1) cur->next = list2;
+        else cur->next = list1;
 
+        return head;
     }
 };
