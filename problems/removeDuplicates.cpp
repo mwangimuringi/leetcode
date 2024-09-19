6e5a5af27967950c7e393e75229015ecc971496c
mwangimuringi <mwangimuringi10@gmail.com>
2024-09-19 12:28:35 +0300

    Using constant space

--- problems/removeDuplicates.cpp
+++ problems/removeDuplicates.cpp
@@ -1,19 +1,22 @@
 //
 // Created by julius on 9/19/2024.
 //remove duplicate from sorted array on ascending order
-//using extra space
+//using constant space
+
+#include <vector>
+using namespace std;
 
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int i = 0;
+        int j = 0 , i = 0;
         for (i = 0, i < n-1, i++);
         {
-            if (nums[i] != nums[i+1]); //array is sorted
+            if (nums[i] != nums[i+1])
             {
-                temp[i] = nums[i];
-            } j++;
+                nums[j] = nums[i];
+                j++;
+            }
         }
     }
-    temp[j] = nums[i];
 };
