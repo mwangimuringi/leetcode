2442976fd9666c01d607f23f2ad4730c72d3096f
mwangimuringi <mwangimuringi10@gmail.com>
2024-09-19 12:11:20 +0300

    Using extra space

---
+++ problems/removeDuplicates.cpp
@@ -0,0 +1,19 @@
+//
+// Created by julius on 9/19/2024.
+//remove duplicate from sorted array on ascending order
+//using extra space
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i = 0;
+        for (i = 0, i < n-1, i++);
+        {
+            if (nums[i] != nums[i+1]); //array is sorted
+            {
+                temp[i] = nums[i];
+            } j++;
+        }
+    }
+    temp[j] = nums[i];
+};
